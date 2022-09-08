76d0383cf5044980e0f416d05cb66ba28e18ac14
inunosinsi <tsuyoshi@saitodev.co>
2022-09-08 13:44:33 +0900

    [Add]Added glider code.

--- redarmor/src/redarmor.cpp
+++ redarmor/src/redarmor.cpp
@@ -1,6 +1,7 @@
 #include <libreborn/libreborn.h>
 #include <symbols/minecraft.h>
 #include <mods/misc/misc.h>
+#include <iostream>
 
 typedef ItemInstance *(*Player_getArmor_t)(unsigned char *player, int32_t slot);
 static Player_getArmor_t Player_getArmor = (Player_getArmor_t) 0x8fda4;
@@ -20,9 +21,9 @@ static void mcpi_callback(unsigned char *minecraft){
     level = *(unsigned char **) (minecraft + Minecraft_level_property_offset);
     if (player != NULL){
         // Uses texture changing to have the red texture work.
-        ItemInstance *item = Player_getArmor(player, 1);
+        ItemInstance *item = Player_getArmor(player, 2);
         if (item == NULL) return;
-        if (item->id == 404){
+        if (item->id == 405){
             *(std::string*) red_1_png = "armor/red_1.png";
         } else {
             *(std::string*) red_1_png = "armor/iron_1.png";
@@ -55,22 +56,22 @@ unsigned char *make_red_chest(){
     return item;
 }
 
-static void Item_initItems_injection(__attribute__((unused)) unsigned char *null) {
+static void Item_initRedChest_injection(__attribute__((unused)) unsigned char *null) {
     red_chest = make_red_chest();
 }
 
 // Add redarmor to creative inventory
-static void Inventory_setupDefault_FillingContainer_addItem_call_injection(unsigned char *filling_container) {
+static void Inventory_setupDefault_FillingContainer_addRedChest_call_injection(unsigned char *filling_container) {
     ItemInstance *red_instance = new ItemInstance;
     ALLOC_CHECK(red_instance);
     red_instance->count = 255;
     red_instance->auxiliary = 0;
-    red_instance->id = 404;
+    red_instance->id = 405;
     (*FillingContainer_addItem)(filling_container, red_instance);
 }
 
 __attribute__((constructor)) static void init() {
     misc_run_on_update(mcpi_callback);
-    misc_run_on_creative_inventory_setup(Inventory_setupDefault_FillingContainer_addItem_call_injection);
-    misc_run_on_items_setup(Item_initItems_injection);
+    misc_run_on_creative_inventory_setup(Inventory_setupDefault_FillingContainer_addRedChest_call_injection);
+    misc_run_on_items_setup(Item_initRedChest_injection);
 }
